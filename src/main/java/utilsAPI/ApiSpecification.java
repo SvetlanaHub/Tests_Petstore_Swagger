2983ae88a417abaaa218f9294d2e1d8898a3ba86
Svetlana Petrovich <petrovichsvetlana@bk.ru>
2021-08-24 23:47:01 +0300

    Added test get login

--- src/main/java/utilsAPI/ApiSpecification.java
+++ src/main/java/utilsAPI/ApiSpecification.java
@@ -4,9 +4,12 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import utils.Log;
 
 
-public class PetShopApiSpecification {
+public class ApiSpecification {
+
+    public static final RequestSpecification APISPECIFICATION = ApiSpecification.getRequestSpecification();
 
     public static RequestSpecification getRequestSpecification() {
         return new RequestSpecBuilder()
---
+++ src/test/java/user/TestGetLogin.java
@@ -0,0 +1,40 @@
+package user;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import static io.restassured.RestAssured.given;
+import static utilsAPI.ApiSpecification.APISPECIFICATION;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+public class TestGetLogin {
+    String baseUri = "https://petstore.swagger.io/v2/user/login?username=Sv-user&password=dok-12";
+
+    @Test
+    @DisplayName("Check login status code")
+    public void testGetLogin() {
+        Response response = given()
+                .spec(APISPECIFICATION)
+                .baseUri(baseUri)
+                .accept(ContentType.JSON)
+                .queryParam("username", "Sv-user")
+                .queryParam("password", "dok-12")
+                .when()
+                .when().get()
+                .then()
+                .extract().response();
+        String statusLine =response.statusLine().substring(13,15);
+        String jsonBody = response.getBody().asString();
+        try {
+            Assertions.assertNotNull(jsonBody);
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+        Assertions.assertEquals(statusLine,"OK");
+    }
+
+}
