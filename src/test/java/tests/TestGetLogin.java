ef3a788edbc916a6a0f6b295ccb0daa064016e6b
Svetlana Petrovich <petrovichsvetlana@bk.ru>
2021-08-25 20:57:38 +0300

    Added test "Checking the order for a pet"

--- src/test/java/tests/TestGetLogin.java
+++ src/test/java/tests/TestGetLogin.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import static io.restassured.RestAssured.given;
 import static utilsAPI.ApiSpecification.REQUEST_SPECIFICATION;
+import static dataConstants.DataConstants.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
@@ -15,19 +16,19 @@ public class TestGetLogin {
     String baseUri = "https://petstore.swagger.io/v2/user/login?username=Sv-user&password=dok-12";
 
     @Test
-    @DisplayName("Check login status code")
+    @DisplayName("Checking user authorization in the system")
     public void testGetLogin() {
         Response response = given()
                 .spec(REQUEST_SPECIFICATION)
                 .baseUri(baseUri)
                 .accept(ContentType.JSON)
-                .queryParam("username", "Sv-user")
-                .queryParam("password", "dok-12")
+                .queryParam("username", USERNAME)
+                .queryParam("password", PASSWORD)
                 .when()
                 .when().get()
                 .then()
                 .extract().response();
-        String statusLine =response.statusLine().substring(13,15);
+        String statusLine = response.statusLine().substring(13,15);
         String jsonBody = response.getBody().asString();
         try {
             Assertions.assertNotNull(jsonBody);
